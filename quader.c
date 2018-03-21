efafa8a218c2eabf9d3e9a583e9d421a4f9ca842
Jakob Roland Schindelwig <csav2783@zid-t9pc1918.uibk.ac.at>
2018-03-21 15:21:26 +0100

    Aufgane-02b

--- quader.c
+++ quader.c
@@ -4,20 +4,28 @@
 int main(int argc, char ** argv) {
 	float a, b, c;
 
+	if(  argc == 4) 
+	{	
 	// Einlesen der Seitenlängen
 	a = atof(argv[1]);
 	b = atof(argv[2]);
 	c = atof(argv[3]);
 
+		
 	// Berechnung der Oberfläche
 	float oberflaeche = 0;
 	oberflaeche += 2.*a*b;
 	oberflaeche += 2.*a*c;
 	oberflaeche += 2.*b*c;
+	
 
 	// Ausgabe
 	printf("Ein Quader mit den Seitenlängen %f, %f und %f hat die Oberfläche %f\n",
 		a, b, c, oberflaeche);
-
+	}
+	else 
+	{
+	printf( "es wurden nicht genau 3 Werte eingegeben" );
+	}
 	return 0;
 }
